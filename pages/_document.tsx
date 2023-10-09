1e1363ad530ce7a560766210049ed3cecb6881a9
Ray Keating <raykeating13@gmail.com>
2023-10-08 22:18:02 -0400

    add favicon, seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,43 @@ export default function Document() {
 					href="https://fonts.cdnfonts.com/css/montserrat"
 					rel="stylesheet"
 				></link>
+				{/* SEO */}
+				<meta name="title" content="ImageSorter.io" />
+				<meta
+					name="description"
+					content="A free tool for sorting and organizing your images"
+				/>
+				
+				{/* Open Graph / Facebook */}
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://imagesorter.io/" />
+				<meta property="og:title" content="ImageSorter.io" />
+				<meta
+					property="og:description"
+					content="A free tool for sorting and organizing your images"
+				/>
+				<meta
+					property="og:image"
+					content="https://imagesorter.io/ImageSorterLogo.png"
+				/>
+				
+				{/* Twitter */}
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://imagesorter.io/" />
+				<meta property="twitter:title" content="ImageSorter.io" />
+				<meta
+					property="twitter:description"
+					content="A free tool for sorting and organizing your images"
+				/>
+
+				<meta
+					property="twitter:image"
+					content="https://imagesorter.io/ImageSorterLogo.png"
+				/>
+
+				{/* Favicon */}
+				<link rel="icon" href="/favicon.ico" />
+
 			</Head>
 			<body>
 				<Main />
