7a98f55fc63a2ad2700116027ad87512de426d0a
Ray Keating <raykeating13@gmail.com>
2023-10-21 13:40:55 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -44,6 +45,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<Component {...pageProps} />
+			<Analytics />
 		</>
 	);
 }
