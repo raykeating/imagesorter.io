82d32ea7ad3abec72680bc9b19314f3fad0e5ab7
Ray Keating <raykeating13@gmail.com>
2023-10-21 13:21:34 -0400

    add vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Document() {
 	return (
@@ -13,48 +14,12 @@ export default function Document() {
 					href="https://fonts.cdnfonts.com/css/montserrat"
 					rel="stylesheet"
 				></link>
-				{/* SEO */}
-
-				<meta name="title" content="ImageSorter.io" />
-				<meta
-					name="description"
-					content="A free tool for sorting and organizing your images"
-				/>
-				
-				{/* Open Graph / Facebook */}
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://imagesorter.io/" />
-				<meta property="og:title" content="ImageSorter.io" />
-				<meta
-					property="og:description"
-					content="A free tool for sorting and organizing your images"
-				/>
-				<meta
-					property="og:image"
-					content="https://imagesorter.io/ImageSorterLogo.png"
-				/>
-				
-				{/* Twitter */}
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://imagesorter.io/" />
-				<meta property="twitter:title" content="ImageSorter.io" />
-				<meta
-					property="twitter:description"
-					content="A free tool for sorting and organizing your images"
-				/>
-
-				<meta
-					property="twitter:image"
-					content="https://imagesorter.io/ImageSorterLogo.png"
-				/>
-
-				{/* Favicon */}
-				<link rel="icon" href="/favicon.ico" />
 
 			</Head>
 			<body>
 				<Main />
 				<NextScript />
+				<Analytics />
 			</body>
 		</Html>
 	);
