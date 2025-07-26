13a9a074bf33306932ca5ed4c455ccb05c41feef
Ray Keating <raykeating13@gmail.com>
2025-07-25 23:43:04 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, Script } from "next/document";
 
 export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<script
+				<Script
 					async
 					src="https://kit.fontawesome.com/c1925a55b2.js"
 					crossOrigin="anonymous"
-				></script>
+				/>
+				
 				<link
 					href="https://fonts.cdnfonts.com/css/montserrat"
 					rel="stylesheet"
