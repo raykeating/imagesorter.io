a6c973585f17d03277f46654ed1be208c98358a4
Ray Keating <raykeating13@gmail.com>
2025-08-02 13:16:22 -0400

    update fontawesome import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,11 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<NextScript
+				<script
 					async
 					src="https://kit.fontawesome.com/c1925a55b2.js"
 					crossOrigin="anonymous"
-				/>
+				></script>
 				
 				<link
 					href="https://fonts.cdnfonts.com/css/montserrat"
